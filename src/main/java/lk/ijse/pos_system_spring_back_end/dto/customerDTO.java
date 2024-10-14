e86049c815315759bec7c51c35a6e1c5e40ee140
pasindu <madushan01722@gmail.com>
2024-10-14 16:52:25 +0530

    create customerDTO

---
+++ src/main/java/lk/ijse/pos_system_spring_back_end/dto/customerDTO.java
@@ -0,0 +1,9 @@
+package lk.ijse.pos_system_spring_back_end.dto;
+
+public class customerDTO {
+    private String cusId;
+    private String cusName;
+    private String cusAddress;
+    private String cusTel;
+    private String cusNIC;
+}
