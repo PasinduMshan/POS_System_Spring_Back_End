f00df06e2d51edb8c97b89f4b8cb9b6d49448d0a
pasindu <madushan01722@gmail.com>
2024-10-17 15:09:49 +0530

    create item save part

--- src/main/java/lk/ijse/pos_system_spring_back_end/controller/ItemController.java
+++ src/main/java/lk/ijse/pos_system_spring_back_end/controller/ItemController.java
@@ -1,10 +1,37 @@
 package lk.ijse.pos_system_spring_back_end.controller;
 
+import lk.ijse.pos_system_spring_back_end.dto.Impl.CustomerDTO;
+import lk.ijse.pos_system_spring_back_end.dto.Impl.ItemDTO;
+import lk.ijse.pos_system_spring_back_end.exception.DataPersistException;
+import lk.ijse.pos_system_spring_back_end.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/item")
 public class ItemController {
+
+    @Autowired
+    ItemService itemService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveItem(@RequestBody ItemDTO itemDTO) {
+        try {
+            itemService.saveItem(itemDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
 
 }
--- src/main/java/lk/ijse/pos_system_spring_back_end/service/impl/ItemServiceImpl.java
+++ src/main/java/lk/ijse/pos_system_spring_back_end/service/impl/ItemServiceImpl.java
@@ -1,9 +1,14 @@
 package lk.ijse.pos_system_spring_back_end.service.impl;
 
+import lk.ijse.pos_system_spring_back_end.dao.ItemDao;
 import lk.ijse.pos_system_spring_back_end.dto.Impl.ItemDTO;
 import lk.ijse.pos_system_spring_back_end.dto.ItemStatus;
+import lk.ijse.pos_system_spring_back_end.entity.Impl.CustomerEntity;
+import lk.ijse.pos_system_spring_back_end.entity.Impl.ItemEntity;
 import lk.ijse.pos_system_spring_back_end.exception.DataPersistException;
 import lk.ijse.pos_system_spring_back_end.service.ItemService;
+import lk.ijse.pos_system_spring_back_end.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,9 +17,19 @@ import java.util.List;
 @Service
 @Transactional
 public class ItemServiceImpl implements ItemService {
+
+    @Autowired
+    ItemDao itemDao;
+
+    @Autowired
+    Mapping itemMapping;
+
     @Override
     public void saveItem(ItemDTO itemDTO) throws DataPersistException {
-
+        ItemEntity saveItem = itemDao.save(itemMapping.toItemEntity(itemDTO));
+        if (saveItem == null) {
+            throw new DataPersistException("Item not saved");
+        }
     }
 
     @Override
